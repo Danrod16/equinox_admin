2dec2cd3ee92c59c95e31b33b6715257321dbe2c
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-02-10 16:51:40 +0100

    Feat: users model ready

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_185113) do
+ActiveRecord::Schema.define(version: 2021_02_10_154326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,6 +132,10 @@ ActiveRecord::Schema.define(version: 2021_01_21_185113) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "role", default: "Agente"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
