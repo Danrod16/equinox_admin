773f2da2ebb6791e92d5ac32f42a43a6c9f950d1
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-02-22 16:35:45 +0100

    Feat: increased the number of records per page

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -16,8 +16,8 @@ module Admin
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
+    def records_per_page
+      params[:per_page] || 70
+    end
   end
 end
