b45c03c28783be80a0d639da3a5afde935b31bfb
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-02-22 16:34:57 +0100

    Feat: added current_user authentication to the app

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,7 +9,9 @@ module Admin
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      if current_user
+        true
+      end
     end
 
     # Override this value to specify the number of elements to display at a time
