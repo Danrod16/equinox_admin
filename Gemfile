aa8424b4d0637eb13e92f8aa730f2b1814ec587c
Alejandro Bringas <bringasalejandro@gmail.com>
2021-03-29 10:56:23 +0200

    Installed Pundit and added models policies

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise'
 gem 'administrate'
 gem 'prawn'
-
+gem "pundit"
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
---
+++ app/policies/flat_policy.rb
@@ -0,0 +1,29 @@
+class FlatPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    @user.admin?
+  end
+end
