b0cdf953b6d0d54a3ffc63a9aabd890f410d1ed9
Alejandro Bringas <bringasalejandro@gmail.com>
2021-03-29 15:36:56 +0200

    Number of payments calculated automatically after create a Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,7 +7,8 @@ class Booking < ApplicationRecord
   has_many :receipts, dependent: :destroy
   has_many :invoices, dependent: :destroy
   has_many :incidents, dependent: :destroy
-  # after_create :create_invoice
+  after_create :create_invoice, :contract_length
+  after_update :contract_length, if: :booking_date_changed?
   after_update :update_invoice
 
   def create_invoice
@@ -20,6 +21,13 @@ class Booking < ApplicationRecord
   end
 
   def contract_length
-    self.end_date.to_date - self.start_date.to_date
+    number_of_payments = (self.end_date.to_date - self.start_date.to_date).to_i / 30
+    self.update(payments: number_of_payments)
+  end
+
+  private
+  
+  def booking_date_changed?
+    self.start_date_changed? || self.end_date_changed?
   end
 end
