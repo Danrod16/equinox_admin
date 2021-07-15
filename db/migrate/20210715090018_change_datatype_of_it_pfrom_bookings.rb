5ef823a3fd7f70f7ea3b636ac64be2f541d94e23
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-07-15 11:07:37 +0200

    Fix: added fixes

--- db/migrate/20210715090018_change_datatype_of_it_pfrom_bookings.rb
+++ db/migrate/20210715090018_change_datatype_of_it_pfrom_bookings.rb
@@ -1,6 +1,8 @@
 class ChangeDatatypeOfItPfromBookings < ActiveRecord::Migration[6.0]
   def change
-    remove_column :bookings, :itp
-    add_column :bookings, :itp, :float
+    remove_column :bookings, :appliences
+    add_column :bookings, :appliences, :float
+    remove_column :bookings, :agency_fee
+    add_column :bookings, :agency_fee, :float
   end
 end
