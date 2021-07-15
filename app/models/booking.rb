9347dafcdc63ba737667a56e9f02d32c884a21f2
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-07-15 11:52:09 +0200

    Fix: added strip_date before update

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -11,6 +11,7 @@ class Booking < ApplicationRecord
   after_create :create_invoice, :contract_length
   before_update :contract_length, if: :booking_date_changed?
   after_create :strip_date
+  before_update :strip_date
 
   def create_invoice
     Invoice.create(booking_id: self.id, state: self.state)
