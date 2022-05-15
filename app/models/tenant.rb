c8f729d2ea10ec84a673ea4dad8e07f71d8ae799
Daniel Rodriguez <daniel.rod93@gmail.com>
2022-05-15 17:55:04 +0800

    Feat: added tenants index

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,6 +1,12 @@
 class Tenant < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
+  include PgSearch::Model
+  pg_search_scope :search_by_first_name_and_last_name_and_address,
+    against: [ :first_name, :last_name, :street ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 
   def full_name
     "#{self.first_name} #{self.last_name}"
