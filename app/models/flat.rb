465e090ceb0d8b7979d08abaad8a6f5aad1113ae
Daniel Rodriguez <daniel.rod93@gmail.com>
2022-05-15 18:27:55 +0800

    Feat: added flats model

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -7,11 +7,18 @@ class Flat < ApplicationRecord
   validates :number, presence: true
   validates :postal_code, presence: true
 
+  include PgSearch::Model
+  pg_search_scope :search_by_name_or_street,
+    against: [ :name, :street],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+
   def full_address
     "#{self.street}, #{self.number} #{self.postal_code}, #{self.city}, #{self.country}"
   end
 
   def table_attribute
-    return self.street
+    return self.name
   end
 end
