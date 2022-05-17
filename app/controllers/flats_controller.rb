1587bc1f48de8b9e8a2f75337ee9c1d198dda230
Christina Sugiono <christina.sugiono95@gmail.com>
2022-05-17 08:15:07 +0800

    added link on buttons to form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -20,6 +20,16 @@ class FlatsController < ApplicationController
     else
       render :new
     end
+
+    # respond_to do |format|
+    #   if @item.save
+    #     format.html { redirect_to @item, notice: 'Item was successfully created.' }
+    #     format.json { render json: @item }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @item.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   private
