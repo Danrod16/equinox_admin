09ed94c30baa4cd6143db872ba5115795a14d70d
Christina Sugiono <christina.sugiono95@gmail.com>
2022-06-15 11:23:33 +0800

    adding collection to status and contract type

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -1,16 +1,2 @@
 class DepositsController < ApplicationController
-  def new
-    @deposit = Deposit.new
-  end
-
-  # def create
-  #   @deposit = Deposit.new(deposit_params)
-  #   @deposit.save
-  # end
-
-  private
-
-  def deposit_params
-    params.require(:deposit).permit(:state, :amount)
-  end
 end
--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,5 +1,8 @@
 class Deposit < ApplicationRecord
+  STATE = ['Abierta', 'Cerrada']
+
   belongs_to :booking
+  validates :amount, :state, :token_payment, presence: true
 
   def table_attribute
     return self.amount
