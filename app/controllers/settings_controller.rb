2a4f587e0203e87ebca133a589a5b9792d59062d
Christina Sugiono <christina.sugiono95@gmail.com>
2022-07-11 15:16:47 +0800

    feat: update user

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -28,5 +28,21 @@ class SettingsController < ApplicationController
   def setting
     @members = User.where(company: current_user.company)
     @company = current_user.company
+    @user = current_user
+  end
+
+  def update_user
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to setting_path
+    else
+      render :setting
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :photo)
   end
 end
