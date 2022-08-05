b1d72de8a4916689d319927945ad4b4204eb6297
Christina Sugiono <christina.sugiono95@gmail.com>
2022-08-05 15:41:50 +0800

    feat: password visibility

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,12 @@
-// Load all the controllers within this directory and all subdirectories. 
+// Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import PasswordVisibility from 'stimulus-password-visibility'
+
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+application.register('password-visibility', PasswordVisibility)
