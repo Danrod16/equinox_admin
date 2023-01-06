bc4860c50fd5d3967c9533e45529b8afc5e9b3a6
Christina Sugiono <christina.sugiono95@gmail.com>
2023-01-06 16:33:57 +0700

    feat: markers with booking status

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,11 +3,18 @@ class SettingsController < ApplicationController
     @flats = Flat.all
 
     @markers = @flats.geocoded.map do |flat|
+      last_booking = flat.bookings.last
+
+      image = if last_booking.present? && last_booking.state == 'Abierta'
+                (last_booking.end_date.to_date > (Date.today + 7.days)) ? helpers.asset_url("home-rent.png") : helpers.asset_url("home-soon.png")
+              else
+                helpers.asset_url("home-available.png")
+              end
       {
         lat: flat.latitude,
         lng: flat.longitude,
         info_window: render_to_string(partial: "flats/info_window", locals: { flat: flat }),
-        image_url: helpers.asset_url("map-pin.svg")
+        image_url: image
       }
     end
 
@@ -15,9 +22,16 @@ class SettingsController < ApplicationController
     @sales_this_year = @sales_monthly.values.sum
     @sales_monthly = @sales_monthly.map { |k, v| [k.to_i, v.to_i] }.to_h
     # state = open / closed
-    @booking_status = Booking.group(:state).count
+    @booking_status = Booking
+                        .where("start_date <= ? AND end_date >= ?", "31-12-#{Time.now.year}", "01-01-#{Time.now.year}")
+                        .group(:state)
+                        .count
+    @user_booking_status = Booking
+                        .where("user_id = ? AND start_date <= ? AND end_date >= ?", current_user.id, "31-12-#{Time.now.year}", "01-01-#{Time.now.year}")
+                        .group(:state)
+                        .count
     @top_five_flats = Flat
-                        .select("flats.*, COUNT(bookings.id) AS flats_count")
+                        .select("flats.*, SUM(bookings.agency_fee) AS flats_count")
                         .joins(:bookings)
                         .group("flats.id")
                         .order("flats_count DESC")
